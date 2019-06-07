dd722ebedf5cbed35138e3a62d67892704d50475
Nishad Pawaskar <nishad.pawaskar@st.ovgu.de>
2019-06-07 18:13:50 +0200

    Filter by Income Category

--- app/src/main/java/com/example/celedger1/IncomeActivity.java
+++ app/src/main/java/com/example/celedger1/IncomeActivity.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 //CLASS FOR INCOME ACTIVITY
 public class IncomeActivity extends AppCompatActivity {
@@ -19,6 +22,11 @@ public class IncomeActivity extends AppCompatActivity {
     SQLiteDatabase incdb;
     IncmeAdaptor icAdaptor;
     float icTotal;
+    int Sosoi;
+    String rq1,rq2, rq3, rq;
+    ArrayList<String> SOI;
+    private static Integer state = 0;
+    public static String TAG = IncomeActivity.class.getSimpleName();
 
     //CREATE LAYOUT
     @Override
@@ -37,16 +45,56 @@ public class IncomeActivity extends AppCompatActivity {
         Home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(SOI != null) {
+                    SOI.clear();
+                }
+                Sosoi = 0;
+                SortIncbyCat.Sizeof = 0;
                 Intent MainIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(MainIntent);
                 finish();
             }
         });
 
+        //FIND NUMBER OF FILTERS
+        SOI = SortIncbyCat.IncCat;
+        Log.d(TAG, "SOI: " + SOI);
+        if(state!=0) {
+            Sosoi = SortIncbyCat.Sizeof;
+            Log.d(TAG, "Size of SOI: " + Sosoi);
+        }
+
+        //CREATE A QUERY TO FILTER DATABASE
+        StringBuilder sb = new StringBuilder();
+        if(Sosoi!=0) {
+            if (Sosoi > 1) {
+                rq1 = CeledgerContract.IncomeEntry.COL_4 + " = ('" + SOI.get(0) + "')";
+                Log.d(TAG, "rq1: " + rq1 + "\n");
+                for (int i = 1; i < Sosoi; i++) {
+                    rq2 = " OR " + CeledgerContract.IncomeEntry.COL_4 + " = ('" + SOI.get(i) + "')";
+                    rq3 = sb.append(rq2).toString();
+                    Log.d(TAG, "rq2: " + rq2 + "\n");
+                    Log.d(TAG, "rq3: " + rq3 + "\n");
+                }
+                rq = rq1 + rq3;
+            }
+            else if(Sosoi == 1){
+                rq = CeledgerContract.IncomeEntry.COL_4 + " = ('" + SOI.get(0) + "')";
+            }
+        }
+        Log.d(TAG, "query: " + rq);
+
         //SHOWS SCROLLABLE INCOME LIST
+        if(Sosoi == 0) {
         IncmelistRCV.setLayoutManager(new LinearLayoutManager(this));
         icAdaptor = new IncmeAdaptor(this, getAllIncome());
         IncmelistRCV.setAdapter(icAdaptor);
+        }
+        else {
+            IncmelistRCV.setLayoutManager(new LinearLayoutManager(this));
+            icAdaptor = new IncmeAdaptor(this, getFilteredIncme());
+            IncmelistRCV.setAdapter(icAdaptor);
+        }
 
         //FILTER BY CATEGORIES
         IcCat.setOnClickListener(new View.OnClickListener() {
@@ -54,22 +102,41 @@ public class IncomeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent OpenicFilter = new Intent(getApplicationContext(),SortIncbyCat.class);
                 startActivity(OpenicFilter);
+                state++;
+                finish();
             }
         });
 
         //SHOW TOTAL INCOME
+        if(Sosoi == 0) {
         Cursor dcursor = incdb.rawQuery("SELECT SUM(" + CeledgerContract.IncomeEntry.COL_5 + ") as Total FROM " + CeledgerContract.IncomeEntry.INCOME_TABLE, null);
         if (dcursor.moveToFirst()) {
             icTotal = dcursor.getFloat(dcursor.getColumnIndex("Total"));// get final total
             }
         dcursor.close();
         Totalincome.setText(String.valueOf(icTotal));
+        }
+        else {
+            Cursor dcursor = incdb.rawQuery("SELECT SUM(" + CeledgerContract.IncomeEntry.COL_5 + ") as Total FROM " + CeledgerContract.IncomeEntry.INCOME_TABLE + " WHERE " + rq, null);
+            if (dcursor.moveToFirst()) {
+                icTotal = dcursor.getFloat(dcursor.getColumnIndex("Total"));// get final total
+            }
+            dcursor.close();
+            Totalincome.setText(String.valueOf(icTotal));
+        }
     }
 
     @Override
     public void onBackPressed() {
+
+        if(SOI != null){
+            SOI.clear();
+        }
+        Sosoi = 0;
+        SortExpbyCat.Sizeof = 0;
         Intent MainIntent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(MainIntent);
+        finish();
         super.onBackPressed();
     }
 
@@ -82,5 +149,10 @@ public class IncomeActivity extends AppCompatActivity {
                 null,
                 CeledgerContract.IncomeEntry.COL_6 +" DESC");
         return cursor;
-        }
+    }
+
+    private Cursor getFilteredIncme(){
+        Cursor cursor = incdb.rawQuery("SELECT * FROM " + CeledgerContract.IncomeEntry.INCOME_TABLE + " WHERE " + rq, null);
+        return cursor;
+    }
 }
--- app/src/main/java/com/example/celedger1/SortIncbyCat.java
+++ app/src/main/java/com/example/celedger1/SortIncbyCat.java
@@ -1,24 +1,99 @@
 package com.example.celedger1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class SortIncbyCat extends AppCompatActivity {
+    //DECLARATION
+    CheckBox check_salary, check_payment, check_others;
+    public static ArrayList<String> IncCat;
+    public static Integer Sizeof;
+    Button Applyfilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sort_incby_cat);
 
+        //DECLARATION & DEFINITION
         ImageView Back = findViewById(R.id.goBack);
+        IncCat = new ArrayList<>();
+        Applyfilter = findViewById(R.id.incAF);
+        check_salary = findViewById(R.id.check_salary);
+        check_payment = findViewById(R.id.check_payment);
+        check_others = findViewById(R.id.check_iothers);
 
+        //GO BACK
         Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent IncAct = new Intent(getApplicationContext(), IncomeActivity.class);
+                startActivity(IncAct);
+                IncCat.clear();
+                Sizeof=0;
                 finish();
             }
         });
+
+        //CHECKBOX RENT
+        check_salary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_salary.isChecked()){
+                    IncCat.add("Salary");
+                }
+                else
+                    IncCat.remove("Salary");
+            }
+        });
+        //CHECKBOX SHOPPING
+        check_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_payment.isChecked()){
+                    IncCat.add("Payment");
+                }
+                else
+                    IncCat.remove("Payment");
+            }
+        });
+        //CHECKBOX OTHERS
+        check_others.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_others.isChecked()){
+                    IncCat.add("Others");
+                }
+                else
+                    IncCat.remove("Others");
+            }
+        });
+
+        //APPLY FILTER
+        Applyfilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IncAct = new Intent(getApplicationContext(), IncomeActivity.class);
+                startActivity(IncAct);
+                Sizeof = IncCat.size();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent IncAct = new Intent(getApplicationContext(), IncomeActivity.class);
+        startActivity(IncAct);
+        IncCat.clear();
+        Sizeof=0;
+        super.onBackPressed();
     }
 }
